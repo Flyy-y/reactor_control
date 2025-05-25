39bc45082c14795551d730a3bdced66406ee64f7
Clément BREISCH <clement@breisch.fr>
2025-05-25 00:08:34 +0000

    battery zz

--- battery/main.lua
+++ battery/main.lua
@@ -287,6 +287,28 @@ local function main()
                         failureCount = 0
                     else
                         print("Recovery failed: " .. tostring(initError))
+                        
+                        -- Write crash report
+                        local crashFile = fs.open("/crash.txt", "w")
+                        crashFile.writeLine("=== BATTERY CONTROLLER CRASH REPORT ===")
+                        crashFile.writeLine("Time: " .. os.date("%Y-%m-%d %H:%M:%S"))
+                        crashFile.writeLine("Reason: Watchdog timeout - no successful send for " .. math.floor(timeSinceLastSend) .. " seconds")
+                        crashFile.writeLine("Last error: " .. tostring(initError))
+                        crashFile.writeLine("Failure count: " .. failureCount)
+                        crashFile.writeLine("Last successful send: " .. math.floor(timeSinceLastSend) .. " seconds ago")
+                        crashFile.writeLine("")
+                        crashFile.writeLine("Action: Restarting computer...")
+                        crashFile.close()
+                        
+                        -- Display restart message
+                        term.setCursorPos(1, 20)
+                        term.setTextColor(colors.red)
+                        print("!!! CRITICAL FAILURE - RESTARTING !!!")
+                        term.setTextColor(colors.white)
+                        sleep(2)
+                        
+                        -- Restart the computer
+                        os.reboot()
                     end
                 end
                 watchdogTimer = os.startTimer(30)
